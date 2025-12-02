4c97972a636d0c72b523778fd0626fcda1846628
Nicole kibe <nicolekibe822@gmail.com>
2025-12-02 08:11:25 +0300

    intialize change

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,26 @@
 #!/usr/bin/env python3
 
-def return_evens(num_list):
-    pass
+def return_evens(sequence):
+    """
+    Returns a list of all even elements from a sequence of integers.
+    
+    Args:
+        sequence: A sequence of integers
+        
+    Returns:
+        A list containing only the even integers from the input sequence
+    """
+    return [n for n in sequence if n % 2 == 0]
 
-def make_exclamation(sentence_list):
-    pass
+
+def make_exclamation(sentences):
+    """
+    Takes a list of sentence strings and returns them with exclamation marks.
+    
+    Args:
+        sentences: A list of sentence strings
+        
+    Returns:
+        A list of sentences with exclamation marks appended
+    """
+    return [sentence + "!" for sentence in sentences]
